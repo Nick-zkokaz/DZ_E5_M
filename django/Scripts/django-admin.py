8bc9fff334acaba4221db78117a12cbc2715c49e
Nick-zkokaz <57746282+Nick-zkokaz@users.noreply.github.com>
2020-06-14 18:05:54 +0300

    one

---
+++ django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\dz_e5_m\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ templates/TextModulE5.py
@@ -0,0 +1,18 @@
+cd /home/user/ .ssh/authorized_keys -можно по гуглить
+каждая строка - это и есть ключ
+пароль root
+.env
+в репозитории не храним уникальные значения
+системная папка приложения PyCharm не содержит
+.idea .env .pyc .vscode - должны быть в GitIgnore
+
+Библиотека
+SECRET_KEY в Django и пароль от базы данных
+
+python-dotenv - библиотека хранения переменных окружения
+
+echo $PYTHONPATH
+export PYTHONPATH='/bin/bash'
+(venv) > whith python - покажет место запуска python
+
+в файле
